b9ed758696f2c15637f89302bf303056e57a1384
ADIL DYER <smile.itsadil@gmail.com>
2024-12-12 00:19:58 +0530

    added port to 0.0.0.0

--- app.py
+++ app.py
@@ -26,4 +26,4 @@ def get_meaning():
         return jsonify({"word": word, "meaning": "No definition found."}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000) 
